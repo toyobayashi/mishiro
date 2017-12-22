4bf1a45171301e93e977dae108ed4bfca778c18b
Toyobayashi <lifenglin314@outlook.com>
2017-12-22 18:42:47 +0800

    update 1.0.0

--- src/js/check.js
+++ src/js/check.js
@@ -43,7 +43,7 @@ function check(progressing){
         return config.resVer;
     }
     if(!config.latestResVer){
-        configurer.configure("latestResVer", 10033300);
+        configurer.configure("latestResVer", 10033400);
     }
 
     let versionFrom = configurer.getConfig().latestResVer;
