535fa7d9ef8de77d239ee06d3ad27998f4ec4a48
Toyobayashi <lifenglin314@outlook.com>
2018-02-27 21:18:37 +0800

    show voice decode progress correctly

---
+++ src/js/main/onAcb.js
@@ -0,0 +1,54 @@
+import path from 'path'
+import fs from 'fs'
+import { getPath } from '../common/getPath.js'
+import { acb2mp3 } from './audio.js'
+
+export default async function (event, acbPath, arg) {
+  let isArr = Array.isArray(acbPath)
+  try {
+    if (isArr) {
+      for (let i = 0; i < acbPath.length; i++) {
+        await acb2mp3(acbPath[i])
+      }
+    } else {
+      await acb2mp3(acbPath)
+    }
+  } catch (err) {
+    throw err
+  }
+  if (arg) {
+    const name = path.parse(acbPath).name
+    let argArr = arg.split('/')
+    if (argArr[argArr.length - 2] === 'live') {
+      let fileName = argArr[argArr.length - 1]
+      fs.renameSync(getPath(`./public/asset/sound/live/${name}.mp3`), getPath(`./public/asset/sound/live/${fileName}`))
+      event.sender.send('acb', arg)
+    } else {
+      event.sender.send('acb', arg)
+    }
+  }
+  if (isArr) {
+    for (let i = 0; i < acbPath.length; i++) {
+      fs.unlinkSync(acbPath[i])
+    }
+  } else {
+    fs.unlinkSync(acbPath)
+  }
+  /* exec(`${getPath()}\\bin\\CGSSAudio.exe ${acbPath}`, (err) => {
+    if (!err) {
+      if (arg) {
+        let argArr = arg.split('/')
+        if (argArr[argArr.length - 2] === 'live') {
+          let fileName = argArr[argArr.length - 1]
+          argArr[argArr.length - 1] = fileName
+          fs.renameSync(getPath(`./public/asset/sound/live/${name}.mp3`), getPath(`./public/asset/sound/live/${fileName}`))
+          event.sender.send('acb', acbPath, arg)
+        } else {
+          event.sender.send('acb', acbPath, arg)
+        }
+      } else {
+        fs.unlinkSync(acbPath)
+      }
+    }
+  }) */
+}
--- src/js/main/service.js
+++ src/js/main/service.js
@@ -1,14 +1,11 @@
 import { ipcMain } from 'electron'
-import fs from 'fs'
-import path from 'path'
-/* import { read } from '../util/fsExtra.js'
-import SQL from './sqlExec.js' */
-import { acb2mp3 } from './audio.js'
-import { getPath } from '../common/getPath.js'
+/* import SQL from './sqlExec.js' */
 import config from './resolveConfig.js'
 import onReadManifest from './onReadManifest.js'
 import onReadMaster from './onReadMaster.js'
 import onQueryManifest from './onQueryManifest.js'
+import onAcb from './onAcb.js'
+import onVoiceDec from './onVoiceDec.js'
 
 let manifestData = {}
 let manifests = []
@@ -27,59 +24,12 @@ ipcMain.on('readMaster', (event, masterFile) => {
   onReadMaster(event, masterFile, manifestData, config)
 })
 
-ipcMain.on('acb', async (event, acbPath, url = '') => {
-  let isArr = Array.isArray(acbPath)
-  try {
-    if (isArr) {
-      for (let i = 0; i < acbPath.length; i++) {
-        await acb2mp3(acbPath[i])
-      }
-    } else {
-      await acb2mp3(acbPath)
-    }
-  } catch (err) {
-    throw err
-  }
-  if (url) {
-    const name = path.parse(acbPath).name
-    let urlArr = url.split('/')
-    if (urlArr[urlArr.length - 2] === 'live') {
-      let fileName = urlArr[urlArr.length - 1]
-      fs.renameSync(getPath(`./public/asset/sound/live/${name}.mp3`), getPath(`./public/asset/sound/live/${fileName}`))
-      event.sender.send('acb', url)
-    } else {
-      event.sender.send('acb', url)
-    }
-  } else {
-    let pathArr = isArr ? acbPath[0].split('\\') : acbPath.split('\\')
-    if (pathArr[pathArr.length - 3] === 'voice') {
-      event.sender.send('voice')
-    }
-  }
-  if (isArr) {
-    for (let i = 0; i < acbPath.length; i++) {
-      fs.unlinkSync(acbPath[i])
-    }
-  } else {
-    fs.unlinkSync(acbPath)
-  }
-  /* exec(`${getPath()}\\bin\\CGSSAudio.exe ${acbPath}`, (err) => {
-    if (!err) {
-      if (url) {
-        let urlArr = url.split('/')
-        if (urlArr[urlArr.length - 2] === 'live') {
-          let fileName = urlArr[urlArr.length - 1]
-          urlArr[urlArr.length - 1] = fileName
-          fs.renameSync(getPath(`./public/asset/sound/live/${name}.mp3`), getPath(`./public/asset/sound/live/${fileName}`))
-          event.sender.send('acb', acbPath, url)
-        } else {
-          event.sender.send('acb', acbPath, url)
-        }
-      } else {
-        fs.unlinkSync(acbPath)
-      }
-    }
-  }) */
+ipcMain.on('acb', (event, acbPath, arg = '') => {
+  onAcb(event, acbPath, arg)
+})
+
+ipcMain.on('voiceDec', (event, acbs) => {
+  onVoiceDec(event, acbs)
 })
 
 export default void 0
