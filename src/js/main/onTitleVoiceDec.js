6484e240cdfcff7703212a355c6559afaff1b97f
Toyobayashi <lifenglin314@outlook.com>
2018-03-01 17:14:45 +0800

    random chara title voice

---
+++ src/js/main/onTitleVoiceDec.js
@@ -0,0 +1,28 @@
+import path from 'path'
+import { remove } from '../util/fsExtra.js'
+import { acb2hca, hca2mp3, readdirAsync } from './audio.js'
+
+export default async function (event, acbs) {
+  let hcaFiles = []
+  let hcaDirs = []
+  for (let i = 0; i < acbs.length; i++) {
+    let acb = acbs[i]
+    let hcadir = await acb2hca(acb)
+    hcaDirs.push(hcadir)
+    let files = await readdirAsync(hcadir)
+    let f = []
+    for (let j = 0; j < files.length; j++) {
+      if (/_4_1[23]\.hca$/.test(files[j])) f.push(path.join(hcadir, files[j]))
+    }
+    hcaFiles = hcaFiles.concat(f)
+  }
+  for (let i = 0; i < hcaFiles.length; i++) {
+    await hca2mp3(hcaFiles[i], path.join(path.parse(hcaFiles[i]).dir, '..', path.parse(hcaFiles[i]).name + '.mp3'))
+    event.sender.send('singleHca', i + 1, hcaFiles.length)
+  }
+  for (let i = 0; i < acbs.length; i++) {
+    // remove(acbs[i])
+    remove(hcaDirs[i])
+  }
+  event.sender.send('voiceEnd')
+}
