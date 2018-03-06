3761364fd2dd16d9605e58e8241356aa0d0a97e9
Toyobayashi <lifenglin314@outlook.com>
2018-03-06 23:32:46 +0800

    change counting file path

--- build/count.js
+++ build/count.js
@@ -5,15 +5,12 @@ const getPath = r => path.join(__dirname, '..', r)
 sourceCount({
   js: [
     getPath('./src/js'),
-    getPath('./src/i18n'),
-    getPath('./src/main.js'),
-    getPath('./src/renderer.js'),
     getPath('./build/pack.js'),
     getPath('./build/count.js'),
     getPath('./build/webpack.config.js'),
     getPath('./build/webpack.dll.config.js')
   ],
-  vue: [getPath('./src/template')],
+  vue: [getPath('./src/vue')],
   css: [getPath('./src/css')],
   html: [getPath('./public/index.html')],
   cpp: [getPath('./src/cpp/hca/hca.cpp')],
