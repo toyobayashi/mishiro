0d7b48ad6c3e4628dba010bdc3feb368a4bbd45c
Toyobayashi <lifenglin314@outlook.com>
2018-03-16 18:54:04 +0800

    lazy

---
+++ bin/mishiro.bat
@@ -0,0 +1,2 @@
+@echo off
+..\node_modules\.bin\electron .. --enable-logging
---
+++ bin/prod.bat
@@ -0,0 +1,5 @@
+@echo off
+set NODE_ENV=production
+echo NODE_ENV = production
+..\node_modules\.bin\webpack --config ..\build\webpack.dll.config.js --progress
+..\node_modules\.bin\webpack --config ..\build\webpack.config.js --progress
