4179b442dd64747ecdad0cddd92affc5703ca713
Toyobayashi <lifenglin314@outlook.com>
2018-06-07 21:13:16 +0800

    [script] update packager

--- script/packager.config.ts
+++ script/packager.config.ts
@@ -35,7 +35,7 @@ function nativeCache (modules: string[], cacheRoot: string) {
   return cache
 }
 
-export const cache = nativeCache(['hca', 'sqlite3'],
+export const cache = nativeCache(['hca-decoder', 'sqlite3'],
   path.join(__dirname, `../cache/electron-v${pkg.devDependencies.electron.match(/[0-9]+\.[0-9]+/)}-win32-${arch}`))
 
 export const packagerOptions: packageManager.Options = {
@@ -44,7 +44,7 @@ export const packagerOptions: packageManager.Options = {
   platform: 'win32',
   arch: arch,
   icon: path.join(__dirname, '../src/res/icon/mishiro.ico'),
-  ignore: /node_modules|build|cache|data|release|download|dist|src|script|public\/img\/card|public\/asset\/sound\/live|public\/asset\/sound\/voice|public\/asset\/score|README|tslint\.json|tsconfig\.json|config\.json|package\.json|package-lock\.json|\.git|\.vscode|binding\.gyp|\.npmrc/,
+  ignore: /node_modules|build|cache|data|release|download|dist|src|script|public\/img\/card|public\/asset\/sound\/live|public\/asset\/sound\/voice|public\/asset\/score|README|tslint\.json|tsconfig\.json|config\.json|package\.json|package-lock\.json|\.git|\.vscode|binding\.gyp/,
   appCopyright: 'Copyright (C) 2017 Toyobayashi',
   download: {
     cache: path.join(os.homedir(), '.electron'),
--- script/packager.ts
+++ script/packager.ts
@@ -17,15 +17,61 @@ const execAsync = (cmd: string, options?: ExecOptions) => {
   }) as Promise<void>
 }
 
+function installCppDependencies (installList: string[], appPath: string) {
+  if (installList.length) {
+    ilog(`[${new Date().toLocaleString()}] Installing C++ native module...`)
+    return execAsync(
+      `npm install ${installList.join(' ')} --no-save --target_arch=${arch} --arch=${arch}`,
+      { cwd: path.join(appPath, './resources/app') }
+    ).then(() => {
+      let removeList: string[] = ['../.npmrc']
+      if (installList.indexOf('sqlite3') !== -1) {
+        ilog(`[${new Date().toLocaleString()}] Resolving 'sqlite3' production code...`)
+        const sqlite3AddonDir = fs.readdirSync(path.join(appPath, './resources/app/node_modules/sqlite3/lib/binding'))[0]
+        const lines = fs.readFileSync(path.join(appPath, './resources/app/node_modules/sqlite3/lib/sqlite3.js'), 'utf8').split(/\r?\n/)
+        lines[3] = `var binding = require('./binding/${sqlite3AddonDir}/node_sqlite3.node');`
+        lines.splice(0, 1)
+        lines.splice(1, 1)
+        fs.writeFileSync(path.join(appPath, './resources/app/node_modules/sqlite3/lib/sqlite3.js'), lines.join('\r\n'), 'utf8')
+
+        removeList = [...removeList, ...[
+          'nan',
+          'sqlite3/node_modules',
+          'sqlite3/build',
+          'sqlite3/deps',
+          'sqlite3/src',
+          'sqlite3/appveyor.yml',
+          'sqlite3/binding.gyp',
+          'sqlite3/CHANGELOG.md',
+          'sqlite3/CONTRIBUTING.md',
+          'sqlite3/electron.sh',
+          'sqlite3/README.md'
+        ]]
+      }
+      if (installList.indexOf('hca-decoder') !== -1) {
+        ilog(`[${new Date().toLocaleString()}] Resolving 'hca-decoder' production code...`)
+        removeList = [...removeList, ...[
+          'hca-decoder/src',
+          'hca-decoder/build',
+          'hca-decoder/binding.gyp',
+          'hca-decoder/README.md'
+        ]]
+      }
+      return Promise.all(removeList.map(p => fs.remove(path.join(appPath, 'resources/app/node_modules', p)))).then(() => {
+        ilog(`[${new Date().toLocaleString()}] Cache C++ native module...`)
+        return Promise.all(
+          installList.map(
+            moduleName => fs.copy(path.join(appPath, './resources/app/node_modules', moduleName), cache[moduleName].path)
+          )
+        )
+      })
+    })
+  }
+  return Promise.all([fs.remove(path.join(appPath, 'resources/app/node_modules', '../.npmrc'))])
+}
+
 const start = new Date().getTime()
 Promise.resolve().then(() => {
-  if (cache.hca.exist) return
-  ilog(`[${new Date().toLocaleString()}] Rebuild hca...`)
-  return execAsync(`npm run hca --arch=${arch}`, { cwd: path.join(__dirname, '..') }).then(() => {
-    ilog(`[${new Date().toLocaleString()}] Cache hca...`)
-    return fs.copy(path.join(__dirname, '../build/Release/hca.node'), path.join(cache.hca.path, 'hca.node'))
-  })
-}).then(() => {
   ilog(`[${new Date().toLocaleString()}] Bundle production code...`)
   return prod(() => process.stdout.write(`[${new Date().toLocaleString()}] `))
 }).then(() => {
@@ -38,46 +84,24 @@ Promise.resolve().then(() => {
 
   ilog(`[${new Date().toLocaleString()}] Write package.json...`)
   return fs.writeFile(path.join(appPaths[0], './resources/app', 'package.json'), JSON.stringify(productionPackage), 'utf8').then(() => {
-    if (!cache.hca.exist) return
-    ilog(`[${new Date().toLocaleString()}] Copy hca...`)
-    return fs.copy(path.join(cache.hca.path, 'hca.node'), path.join(appPaths[0], './resources/app/public/addon/hca.node'))
-  }).then(() => {
-    if (!cache.sqlite3.exist) {
-      ilog(`[${new Date().toLocaleString()}] Installing sqlite3...`)
-      return execAsync(
-        `npm install sqlite3 --no-save --build-from-source --runtime=electron --target=${pkg.devDependencies.electron.replace(/\^|~/, '')} --target_arch=${arch} --dist-url=https://atom.io/download/electron`,
-        { cwd: path.join(appPaths[0], './resources/app') }
-      ).then(() => {
-        ilog(`[${new Date().toLocaleString()}] Resolving 'sqlite3' production code...`)
-        const sqlite3AddonDir = fs.readdirSync(path.join(appPaths[0], './resources/app/node_modules/sqlite3/lib/binding'))[0]
-        const lines = fs.readFileSync(path.join(appPaths[0], './resources/app/node_modules/sqlite3/lib/sqlite3.js'), 'utf8').split(/\r?\n/)
-        lines[3] = `var binding = require('./binding/${sqlite3AddonDir}/node_sqlite3.node');`
-        lines.splice(0, 1)
-        lines.splice(1, 1)
-        fs.writeFileSync(path.join(appPaths[0], './resources/app/node_modules/sqlite3/lib/sqlite3.js'), lines.join('\r\n'), 'utf8')
-
-        ilog(`[${new Date().toLocaleString()}] Clean 'node_modules'...`)
-        const removeList: string[] = [
-          'nan',
-          'sqlite3/node_modules',
-          'sqlite3/build',
-          'sqlite3/deps',
-          'sqlite3/src',
-          'sqlite3/appveyor.yml',
-          'sqlite3/binding.gyp',
-          'sqlite3/CHANGELOG.md',
-          'sqlite3/CONTRIBUTING.md',
-          'sqlite3/electron.sh',
-          'sqlite3/README.md'
-        ]
-        return Promise.all(removeList.map(p => fs.remove(path.join(appPaths[0], 'resources/app/node_modules', p)))).then(() => {
-          ilog(`[${new Date().toLocaleString()}] Cache sqlite3...`)
-          return fs.copy(path.join(appPaths[0], './resources/app/node_modules/sqlite3'), cache.sqlite3.path)
-        })
-      })
+    const installList: string[] = []
+    const cacheList: string[] = []
+    // const promiseArr: PromiseLike<any>[] = []
+    for (const moduleName in cache) {
+      if (!cache[moduleName].exist) installList.push(moduleName)
+      else cacheList.push(moduleName)
     }
-    ilog(`[${new Date().toLocaleString()}] Copy sqlite3...`)
-    return fs.copy(path.join(cache.sqlite3.path), path.join(appPaths[0], './resources/app/node_modules/sqlite3'))
+    return installCppDependencies(installList, appPaths[0]).then(() => {
+      if (cacheList.length) {
+        ilog(`[${new Date().toLocaleString()}] Copy Modules...`)
+        return Promise.all(
+          cacheList.map(
+            moduleName => fs.copy(cache[moduleName].path, path.join(appPaths[0], './resources/app/node_modules', moduleName))
+          )
+        )
+      }
+      return []
+    })
   }).then(() => {
     if (!fs.existsSync(newPath)) return
     wlog(`[${new Date().toLocaleString()}] Overwriting... `)
