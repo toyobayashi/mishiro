938715b90a077281b23fc58bc80f88857526ab6c
toyobayashi <lifenglin314@outlook.com>
2018-07-30 23:26:16 +0800

    [script] remove --enable-logging

--- app/script/start.ts
+++ app/script/start.ts
@@ -1,6 +1,6 @@
 import { spawn } from 'child_process'
 import { join } from 'path'
 
-let cp = spawn(require('electron'), [join(__dirname, '../public/mishiro.main.js'), '--enable-logging'])
+let cp = spawn(require('electron'), [join(__dirname, '../public/mishiro.main.js')])
 cp.stdout.pipe(process.stdout)
 cp.stderr.pipe(process.stderr)
