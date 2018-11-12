1caeb9771eb962df9939c7e695c816061aca7931
toyobayashi <lifenglin314@outlook.com>
2018-11-12 21:42:56 +0800

    [renderer] fix game parent window

--- app/src/ts/renderer-game.ts
+++ app/src/ts/renderer-game.ts
@@ -1,20 +1,19 @@
 import '../css/game.css'
-import { remote, ipcRenderer, Event } from 'electron'
-import { parse, relative } from 'path'
-import { Note, ShortNote, LongNote, newImage, keyBind, liveResult } from './renderer/game'
+import { ipcRenderer, Event } from 'electron'
+
+import { Game, newImage, keyBind } from './renderer/game'
 import Vue from 'vue'
-import Game from '../vue/MishiroGame.vue'
-const prefix = 60
+import MishiroGame from '../vue/MishiroGame.vue'
 
 window.addEventListener('load', () => {
   keyBind()
   let canvasLive = document.getElementById('live') as HTMLCanvasElement
 
-  Note.CTX = canvasLive.getContext('2d') as CanvasRenderingContext2D
+  Game.CTX = canvasLive.getContext('2d') as CanvasRenderingContext2D
   let canvasIconBar = document.getElementById('iconBar') as HTMLCanvasElement
 
-  Note.BACK_CTX = canvasIconBar.getContext('2d') as CanvasRenderingContext2D
-  let ctxIconBar = Note.BACK_CTX
+  Game.BACK_CTX = canvasIconBar.getContext('2d') as CanvasRenderingContext2D
+  let ctxIconBar = Game.BACK_CTX
   let liveIcon = newImage('../../asset/img.asar/live_icon_857x114.png')
   liveIcon.addEventListener('load', function () {
     ctxIconBar.drawImage(this, 211.5, 586)
@@ -23,39 +22,11 @@ window.addEventListener('load', () => {
 }, false)
 
 ipcRenderer.on('start', (_event: Event, song: any, fromWindowId: number) => {
-  let name = parse(song.src).name.split('-')[1]
-  document.getElementsByTagName('title')[0].innerHTML = liveResult.name = name
-  liveResult.fullCombo = song.fullCombo
-  let isCompleted = false
-  window.addEventListener('beforeunload', () => {
-    const fromWindow = remote.BrowserWindow.fromId(fromWindowId)
-    fromWindow.webContents.send('liveEnd', liveResult, isCompleted)
-  })
-  let music = process.env.NODE_ENV === 'production' ? new Audio(song.src) : new Audio(relative(__dirname, song.src))
-  const msbp = 60 / song.bpm * 1000
-  const DELAY = Note.DISTANCE / Note.PX_SPEED
-  music.addEventListener('play', () => {
-    for (let i = 0; i < song.score.length; i++) {
-      const note = song.score[i]
-      setTimeout(() => {
-        // tslint:disable-next-line:no-unused-expression
-        if (!note[2]) new ShortNote(note[1])
-        // tslint:disable-next-line:no-unused-expression
-        else new LongNote(note[1], note[2] * msbp * Note.PX_SPEED)
-      }, note[0] * msbp - DELAY + prefix)
-    }
-  }, false)
-
-  music.addEventListener('ended', () => {
-    isCompleted = true
-    window.close()
-  }, false)
-
-  music.play().catch(err => console.log(err))
+  Game.start(song, fromWindowId)
 })
 
 // tslint:disable-next-line:no-unused-expression
 new Vue({
   el: '#app',
-  render: h => h(Game)
+  render: h => h(MishiroGame)
 })
--- app/src/ts/renderer/game.ts
+++ app/src/ts/renderer/game.ts
@@ -1,3 +1,6 @@
+import { parse, relative } from 'path'
+import { remote } from 'electron'
+
 let liveResult: any = {
   perfect: 0,
   great: 0,
@@ -88,19 +91,53 @@ function newImage (src: string) {
   return img
 }
 
+class Game {
+  public static CTX: CanvasRenderingContext2D
+  public static BACK_CTX: CanvasRenderingContext2D
+  public static IMG: HTMLImageElement = newImage('../../asset/img.asar/icon_notes.png')
+  public static SPEED: number = 12 // x 60 px / s
+  public static PX_SPEED: number = Game.SPEED * 60 / 1000
+  public static TOP_TO_BOTTOM: number = 592
+  public static X: number[] = [238.5, 414.5, 589.5, 764.5, 937.5]
+  public static queue: any[][] = [[], [], [], [], []]
+  public static W: number = 102
+  public static H: number = 102
+  public static DISTANCE: number = Game.TOP_TO_BOTTOM + Game.H
+  public static RANGE: number = 100
+  public static start (song: any, fromWindowId: number) {
+    const prefix = 80
+    let isCompleted = false
+    window.addEventListener('beforeunload', () => {
+      const fromWindow = remote.BrowserWindow.fromId(fromWindowId)
+      fromWindow.webContents.send('liveEnd', liveResult, isCompleted)
+    })
+
+    let name = parse(song.src).name.split('-')[1]
+    document.getElementsByTagName('title')[0].innerHTML = liveResult.name = name
+    liveResult.fullCombo = song.fullCombo
+    let music = process.env.NODE_ENV === 'production' ? new Audio(song.src) : new Audio(relative(__dirname, song.src))
+    const msbp = 60 / song.bpm * 1000
+    const DELAY = Game.DISTANCE / Game.PX_SPEED
+    music.addEventListener('play', () => {
+      for (let i = 0; i < song.score.length; i++) {
+        const note = song.score[i]
+        setTimeout(() => {
+          if (!note[2]) ShortNote.new(note[1])
+          else LongNote.new(note[1], note[2] * msbp * Game.PX_SPEED)
+        }, note[0] * msbp - DELAY + prefix)
+      }
+    }, false)
+
+    music.addEventListener('ended', () => {
+      isCompleted = true
+      window.close()
+    }, false)
+
+    music.play().catch(err => console.log(err))
+  }
+}
+
 class Note {
-  static CTX: CanvasRenderingContext2D
-  static BACK_CTX: CanvasRenderingContext2D
-  static IMG: HTMLImageElement = newImage('../../asset/img.asar/icon_notes.png')
-  static SPEED: number = 12 // x 60 px / s
-  static PX_SPEED: number = Note.SPEED * 60 / 1000
-  static TOP_TO_BOTTOM: number = 592
-  static X: number[] = [238.5, 414.5, 589.5, 764.5, 937.5]
-  static queue: any[][] = [[], [], [], [], []]
-  static W: number = 102
-  static H: number = 102
-  static DISTANCE: number = Note.TOP_TO_BOTTOM + Note.H
-  static RANGE: number = 100
   path: number
   exist: boolean
   x: number
@@ -108,43 +145,47 @@ class Note {
   constructor (path: number, distance: number) {
     this.path = path
     this.exist = true
-    this.x = Note.X[this.path - 1]
-    this.y = Note.TOP_TO_BOTTOM - distance
-    Note.queue[this.path - 1].push(this)
+    this.x = Game.X[this.path - 1]
+    this.y = Game.TOP_TO_BOTTOM - distance
+    Game.queue[this.path - 1].push(this)
   }
 }
 
 class ShortNote extends Note {
-  static IMG_POSITION_X: number = 0
-  static IMG_POSITION_Y: number = 0
+  private static IMG_POSITION_X: number = 0
+  private static IMG_POSITION_Y: number = 0
   t: number
-  constructor (path: number, distance: number = Note.DISTANCE, doDrop: boolean = true) {
+  constructor (path: number, distance: number = Game.DISTANCE, doDrop: boolean = true) {
     super(path, distance)
     this.draw()
     if (doDrop) this.startDrop()
   }
 
+  public static new (path: number, distance: number = Game.DISTANCE, doDrop: boolean = true) {
+    return new ShortNote(path, distance, doDrop)
+  }
+
   clean () {
-    Note.CTX.clearRect(this.x, this.y, Note.W, Note.H)
+    Game.CTX.clearRect(this.x, this.y, Game.W, Game.H)
   }
 
   draw () {
-    Note.CTX.drawImage(
-      Note.IMG,
+    Game.CTX.drawImage(
+      Game.IMG,
       ShortNote.IMG_POSITION_X,
       ShortNote.IMG_POSITION_Y,
-      Note.W,
-      Note.H,
+      Game.W,
+      Game.H,
       this.x,
       this.y,
-      Note.W,
-      Note.H
+      Game.W,
+      Game.H
     )
   }
 
   update (distance: number) {
     this.clean()
-    this.y = Note.TOP_TO_BOTTOM - distance
+    this.y = Game.TOP_TO_BOTTOM - distance
     this.draw()
   }
 
@@ -152,28 +193,28 @@ class ShortNote extends Note {
     this.t = requestAnimationFrame(frame)
     let self = this
     function frame () {
-      if (self.y >= Note.TOP_TO_BOTTOM + Note.RANGE * Note.PX_SPEED) {
+      if (self.y >= Game.TOP_TO_BOTTOM + Game.RANGE * Game.PX_SPEED) {
         self.exist = false
         cancelAnimationFrame(self.t)
         self.clean()
-        Note.queue[self.path - 1].shift()
+        Game.queue[self.path - 1].shift()
         miss()
       } else if (self.exist) {
-        let d = Note.TOP_TO_BOTTOM - self.y
-        self.update(d - Note.SPEED)
-        requestAnimationFrame(frame)
+        let d = Game.TOP_TO_BOTTOM - self.y
+        self.update(d - Game.SPEED)
+        self.t = requestAnimationFrame(frame)
       }
     }
   }
 }
 
 class LongNote extends Note {
-  static IMG_POSITION_X: number = 102
-  static IMG_POSITION_Y: number = 0
+  private static IMG_POSITION_X: number = 102
+  private static IMG_POSITION_Y: number = 0
   status: number
   length: number
   t: number
-  constructor (path: number, length: number, distance: number = Note.DISTANCE, doDrop: boolean = true) {
+  constructor (path: number, length: number, distance: number = Game.DISTANCE, doDrop: boolean = true) {
     super(path, distance)
     this.status = 0
     this.length = length
@@ -181,47 +222,51 @@ class LongNote extends Note {
     if (doDrop) this.startDrop()
   }
 
+  public static new (path: number, length: number, distance: number = Game.DISTANCE, doDrop: boolean = true) {
+    return new LongNote(path, length, distance, doDrop)
+  }
+
   clean () {
-    Note.CTX.clearRect(this.x, this.y - this.length, Note.W + 1, this.length > 0 ? this.length + Note.H : Note.H)
+    Game.CTX.clearRect(this.x, this.y - this.length, Game.W + 1, this.length > 0 ? this.length + Game.H : Game.H)
   }
 
   draw () {
-    Note.CTX.beginPath()
-    Note.CTX.arc(this.x + Note.W / 2, this.y + Note.W / 2, Note.W / 2, Math.PI, 2 * Math.PI, true)
-    Note.CTX.lineTo(this.x + 102, this.y + Note.W / 2 - this.length)
-    Note.CTX.arc(this.x + Note.W / 2, this.y + Note.W / 2 - this.length, Note.W / 2, 0, Math.PI, true)
-    Note.CTX.lineTo(this.x, this.y + Note.W / 2)
-    Note.CTX.fillStyle = 'rgba(255, 255, 255, 0.66)'
-    Note.CTX.fill()
-    Note.CTX.drawImage(
-      Note.IMG,
+    Game.CTX.beginPath()
+    Game.CTX.arc(this.x + Game.W / 2, this.y + Game.W / 2, Game.W / 2, Math.PI, 2 * Math.PI, true)
+    Game.CTX.lineTo(this.x + 102, this.y + Game.W / 2 - this.length)
+    Game.CTX.arc(this.x + Game.W / 2, this.y + Game.W / 2 - this.length, Game.W / 2, 0, Math.PI, true)
+    Game.CTX.lineTo(this.x, this.y + Game.W / 2)
+    Game.CTX.fillStyle = 'rgba(255, 255, 255, 0.66)'
+    Game.CTX.fill()
+    Game.CTX.drawImage(
+      Game.IMG,
       LongNote.IMG_POSITION_X,
       LongNote.IMG_POSITION_Y,
-      Note.W,
-      Note.H,
+      Game.W,
+      Game.H,
       this.x,
       this.y - this.length,
-      Note.W,
-      Note.H
+      Game.W,
+      Game.H
     )
     if (this.status === 0) {
-      Note.CTX.drawImage(
-        Note.IMG,
+      Game.CTX.drawImage(
+        Game.IMG,
         LongNote.IMG_POSITION_X,
         LongNote.IMG_POSITION_Y,
-        Note.W,
-        Note.H,
+        Game.W,
+        Game.H,
         this.x,
         this.y,
-        Note.W,
-        Note.H
+        Game.W,
+        Game.H
       )
     }
   }
 
   update (distance: number, length: number) {
     this.clean()
-    this.y = Note.TOP_TO_BOTTOM - distance
+    this.y = Game.TOP_TO_BOTTOM - distance
     this.length = length
     this.draw()
   }
@@ -230,45 +275,45 @@ class LongNote extends Note {
     this.t = requestAnimationFrame(frame)
     let self = this
     function frame () {
-      if (self.y >= Note.TOP_TO_BOTTOM + Note.RANGE * Note.PX_SPEED) {
+      if (self.y >= Game.TOP_TO_BOTTOM + Game.RANGE * Game.PX_SPEED) {
         self.exist = false
         cancelAnimationFrame(self.t)
         self.clean()
-        Note.queue[self.path - 1].shift()
+        Game.queue[self.path - 1].shift()
         miss()
         miss()
         return
       }
 
       if (self.status === 0 && self.exist) {
-        let d = Note.TOP_TO_BOTTOM - self.y
-        self.update(d - Note.SPEED, self.length)
-        requestAnimationFrame(frame)
+        let d = Game.TOP_TO_BOTTOM - self.y
+        self.update(d - Game.SPEED, self.length)
+        self.t = requestAnimationFrame(frame)
       } else if (self.status === 1 && self.exist) {
-        if (self.y - self.length >= Note.TOP_TO_BOTTOM + Note.RANGE * Note.PX_SPEED) {
+        if (self.y - self.length >= Game.TOP_TO_BOTTOM + Game.RANGE * Game.PX_SPEED) {
           self.exist = false
           cancelAnimationFrame(self.t)
           self.clean()
-          Note.queue[self.path - 1].shift()
+          Game.queue[self.path - 1].shift()
           miss()
           se.longLoop.pause()
           return
         }
         let l = self.length
-        self.update(0, l - Note.SPEED)
-        requestAnimationFrame(frame)
+        self.update(0, l - Game.SPEED)
+        self.t = requestAnimationFrame(frame)
       }
     }
   }
 }
 
 function tap (note: ShortNote) {
-  let dt = Math.abs(note.y - Note.TOP_TO_BOTTOM) / Note.PX_SPEED
-  if (dt <= Note.RANGE) {
+  let dt = Math.abs(note.y - Game.TOP_TO_BOTTOM) / Game.PX_SPEED
+  if (dt <= Game.RANGE) {
     note.exist = false
     cancelAnimationFrame(note.t)
     note.clean()
-    Note.queue[note.path - 1].shift()
+    Game.queue[note.path - 1].shift()
     rank(dt)
   } else {
     playSe(se.clap)
@@ -277,12 +322,12 @@ function tap (note: ShortNote) {
 
 function down (note: LongNote) {
   if (note.status === 0) {
-    let dt = Math.abs(note.y - Note.TOP_TO_BOTTOM) / Note.PX_SPEED
-    if (dt <= Note.RANGE) {
+    let dt = Math.abs(note.y - Game.TOP_TO_BOTTOM) / Game.PX_SPEED
+    if (dt <= Game.RANGE) {
       note.clean()
       rank(dt)
       note.status = 1
-      note.length = note.length - (note.y - Note.TOP_TO_BOTTOM)
+      note.length = note.length - (note.y - Game.TOP_TO_BOTTOM)
       se.longLoop.loop = true
       se.longLoop.play().catch(err => console.log(err))
     } else {
@@ -293,18 +338,18 @@ function down (note: LongNote) {
 
 function up (note: LongNote) {
   se.longLoop.pause()
-  let dt = Math.abs(note.y - note.length - Note.TOP_TO_BOTTOM) / Note.PX_SPEED
-  if (dt <= Note.RANGE) {
+  let dt = Math.abs(note.y - note.length - Game.TOP_TO_BOTTOM) / Game.PX_SPEED
+  if (dt <= Game.RANGE) {
     note.exist = false
     cancelAnimationFrame(note.t)
     note.clean()
-    Note.queue[note.path - 1].shift()
+    Game.queue[note.path - 1].shift()
     rank(dt)
   } else { // miss
     note.exist = false
     cancelAnimationFrame(note.t)
     note.clean()
-    Note.queue[note.path - 1].shift()
+    Game.queue[note.path - 1].shift()
     miss()
   }
 }
@@ -355,7 +400,7 @@ function hpCheck () {
 }
 
 function clearRank () {
-  Note.BACK_CTX.clearRect(0, Note.TOP_TO_BOTTOM - 100, 1280, 54)
+  Game.BACK_CTX.clearRect(0, Game.TOP_TO_BOTTOM - 100, 1280, 54)
 }
 
 let t = 1
@@ -370,7 +415,7 @@ function showRank (rank: string) {
   function show () {
     clearRank()
     if (rank === 'perfect') {
-      Note.BACK_CTX.drawImage(
+      Game.BACK_CTX.drawImage(
         rankImg,
         0,
         212,
@@ -378,56 +423,56 @@ function showRank (rank: string) {
         54,
         // (1280 - 253) / 2,
         (1280 - f * 253 / 9) / 2,
-        // Note.TOP_TO_BOTTOM - 100,
-        Note.TOP_TO_BOTTOM - 75 - f * 25 / 9,
+        // Game.TOP_TO_BOTTOM - 100,
+        Game.TOP_TO_BOTTOM - 75 - f * 25 / 9,
         f * 253 / 9,
         f * 54 / 9
       )
     } else if (rank === 'great') {
-      Note.BACK_CTX.drawImage(
+      Game.BACK_CTX.drawImage(
         rankImg,
         0,
         159,
         208,
         53,
         (1280 - f * 208 / 9) / 2,
-        Note.TOP_TO_BOTTOM - 75 - f * 25 / 9,
+        Game.TOP_TO_BOTTOM - 75 - f * 25 / 9,
         f * 208 / 9,
         f * 53 / 9
       )
     } else if (rank === 'nice') {
-      Note.BACK_CTX.drawImage(
+      Game.BACK_CTX.drawImage(
         rankImg,
         0,
         106,
         141,
         53,
         (1280 - f * 141 / 9) / 2,
-        Note.TOP_TO_BOTTOM - 75 - f * 25 / 9,
+        Game.TOP_TO_BOTTOM - 75 - f * 25 / 9,
         f * 141 / 9,
         f * 53 / 9
       )
     } else if (rank === 'bad') {
-      Note.BACK_CTX.drawImage(
+      Game.BACK_CTX.drawImage(
         rankImg,
         0,
         53,
         128,
         53,
         (1280 - f * 128 / 9) / 2,
-        Note.TOP_TO_BOTTOM - 75 - f * 25 / 9,
+        Game.TOP_TO_BOTTOM - 75 - f * 25 / 9,
         f * 128 / 9,
         f * 53 / 9
       )
     } else if (rank === 'miss') {
-      Note.BACK_CTX.drawImage(
+      Game.BACK_CTX.drawImage(
         rankImg,
         0,
         0,
         151,
         53,
         (1280 - f * 151 / 9) / 2,
-        Note.TOP_TO_BOTTOM - 75 - f * 25 / 9,
+        Game.TOP_TO_BOTTOM - 75 - f * 25 / 9,
         f * 151 / 9,
         f * 53 / 9
       )
@@ -446,7 +491,7 @@ function showRank (rank: string) {
 }
 
 function keydown (path: number) {
-  let note = Note.queue[path - 1][0]
+  let note = Game.queue[path - 1][0]
   if (note) {
     if (note.constructor === ShortNote) tap(note)
     else down(note)
@@ -456,7 +501,7 @@ function keydown (path: number) {
 }
 
 function keyup (path: number) {
-  let note = Note.queue[path - 1][0]
+  let note = Game.queue[path - 1][0]
   if (note && note.constructor === LongNote && note.status === 1) up(note)
 }
 
@@ -484,4 +529,4 @@ function keyBind () {
   }, false)
 }
 
-export { Note, ShortNote, LongNote, newImage, keyBind, liveResult }
+export { Game, Note, ShortNote, LongNote, newImage, keyBind, liveResult }
