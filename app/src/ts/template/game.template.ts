30f3835fb94c74aed6a6baa85c39be1e6ce603ff
toyobayashi <lifenglin314@outlook.com>
2018-11-13 20:02:35 +0800

    [script] webpack config

--- app/src/ts/template/game.template.ts
+++ app/src/ts/template/game.template.ts
@@ -3,12 +3,9 @@ export default `<!DOCTYPE html>
 <head>
   <meta charset="UTF-8">
   <title></title>
-  ${process.env.NODE_ENV === 'production' ? '<link id="css" rel="stylesheet" type="text/css" href="./mishiro.live.css"/>' : ''}
 </head>
 <body>
   <div id="app"></div>
-  <script src="./dll.js"></script>
-  <script src="./mishiro.live.js"></script>
 </body>
 </html>
 `
--- app/src/ts/template/index.template.ts
+++ app/src/ts/template/index.template.ts
@@ -3,12 +3,9 @@ export default `<!DOCTYPE html>
   <head>
     <title>mishiro</title>
     <meta charset="utf-8"/>
-    ${process.env.NODE_ENV === 'production' ? '<link id="css" rel="stylesheet" type="text/css" href="./mishiro.renderer.css"/>' : ''}
   </head>
   <body>
     <div id="app"></div>
-    <script src="./dll.js"></script>
-    <script src="./mishiro.renderer.js"></script>
   </body>
 </html>
 `
