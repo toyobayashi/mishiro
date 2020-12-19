1fcd710a2c722bf8484eb2acef884ff738557357
toyobayashi <lifenglin314@outlook.com>
2020-12-19 17:27:46 +0800

    calculate file hash

---
+++ app/src/ts/main/hash.ts
@@ -0,0 +1,41 @@
+
+import { Writable, WritableOptions } from 'stream'
+import { createReadStream } from 'fs-extra'
+import { createHash } from 'crypto'
+
+class HashResult extends Writable {
+  private _value: string
+  private _defer: {
+    resolve: (value: string | PromiseLike<string>) => void
+    reject: (reason?: any) => void
+  }
+
+  public readonly promise: Promise<string>
+  public constructor (o?: WritableOptions) {
+    super(o)
+    this._value = ''
+    Object.defineProperty(this, 'promise', {
+      enumerable: true,
+      configurable: true,
+      writable: false,
+      value: new Promise<string>((resolve, reject) => {
+        this._defer = { resolve, reject }
+        this.once('error', reject)
+      })
+    })
+  }
+
+  _write (chunk: any, _encoding: string, cb: (err?: Error) => void): void {
+    this._value = chunk.toString('hex')
+    cb()
+  }
+
+  _final (cb: (err?: Error) => void): void {
+    this._defer.resolve(this._value)
+    cb()
+  }
+}
+
+export function md5File (path: string): Promise<string> {
+  return createReadStream(path).pipe(createHash('md5')).pipe(new HashResult()).promise
+}
