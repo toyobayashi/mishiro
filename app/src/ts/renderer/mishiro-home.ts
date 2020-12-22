0d0728f0791f251bca5113bc6fe18cc0ab99c36a
toyobayashi <lifenglin314@outlook.com>
2020-12-22 16:25:10 +0800

    got 11

--- app/src/ts/renderer/mishiro-home.ts
+++ app/src/ts/renderer/mishiro-home.ts
@@ -35,6 +35,8 @@ export default class extends Vue {
   notDownloadedOnly: boolean = false
   canDownloadRows: any[] = []
 
+  manualStop = false
+
   get totalPage (): number {
     const canDownload: any[] = this.canDownloadRows
     if (!canDownload.length) return 0
@@ -127,6 +129,7 @@ export default class extends Vue {
     this.total = 0
     this.current = 0
     this.text = ''
+    this.manualStop = true
     this.dler.stop(() => this.event.$emit('alert', this.$t('home.errorTitle'), this.$t('home.noTask')))
   }
 
@@ -144,16 +147,19 @@ export default class extends Vue {
       const errorList = await this.dler.batchDownload(
         tasks,
         downloadDir(),
+        // onStart
         (_row, filepath) => {
           this.current = 0
           this.total = 100 * this.dler.index / tasks.length
           this.text = path.basename(filepath)
         },
+        // onData
         prog => {
           this.text = `${prog.name}　${Math.ceil(prog.current / 1024)}/${Math.ceil(prog.max / 1024)} KB`
           this.current = prog.loading
           this.total = 100 * this.dler.index / tasks.length + prog.loading / tasks.length
         },
+        // onComplete
         (row, filepath) => {
           // console.log(row.name)
           const name = path.basename(filepath)
@@ -171,59 +177,19 @@ export default class extends Vue {
             else this.event.$emit('completeTask', name, false)
           }
         },
+        // onStop
         () => {
           this.current = 0
           this.text = ''
         }
       )
-      // let taskArr: string[][] = []
-      // for (let i = 0; i < task.length; i++) {
-      //   if (path.extname(task[i].name) === '.acb') {
-      //     taskArr.push([this.getAcbUrl(path.dirname(task[i].name), task[i].hash), downloadDir(path.basename(task[i].name)), 'acb'])
-      //   } else if (path.extname(task[i].name) === '.unity3d') {
-      //     taskArr.push([this.getUnityUrl(task[i].hash), downloadDir(path.basename(task[i].name, '.unity3d')), 'unity3d'])
-      //   } else if (path.extname(task[i].name) === '.bdb') {
-      //     taskArr.push([this.getDbUrl(task[i].hash), downloadDir(path.basename(task[i].name, '.bdb')), 'bdb'])
-      //   } else if (path.extname(task[i].name) === '.mdb') {
-      //     taskArr.push([this.getDbUrl(task[i].hash), downloadDir(path.basename(task[i].name, '.mdb')), 'mdb'])
-      //   }
-      // }
-
-      // let failedList = await this.dler.download(taskArr, ([_url, filepath]) => {
-      //   this.current = 0
-      //   this.total = 100 * this.dler.index / taskArr.length
-      //   this.text = path.basename(filepath)
-      // }, (prog: ProgressInfo) => {
-      //   this.text = `${prog.name}　${Math.ceil(prog.current / 1024)}/${Math.ceil(prog.max / 1024)} KB`
-      //   this.current = prog.loading
-      //   this.total = 100 * this.dler.index / taskArr.length + prog.loading / taskArr.length
-      // }, ([_url, filepath, suffix]) => {
-      //   const name = path.basename(filepath)
-      //   this.current = 0
-      //   this.text = ''
-      //   if (suffix !== 'acb') {
-      //     if (fs.existsSync(filepath)) {
-      //       if (suffix === 'unity3d') {
-      //         this.core.util.lz4dec(filepath, 'unity3d')
-      //       } else if (suffix === 'bdb') {
-      //         this.core.util.lz4dec(filepath, 'bdb')
-      //       } else if (suffix === 'mdb') {
-      //         this.core.util.lz4dec(filepath, 'mdb')
-      //       }
-      //       fs.unlinkSync(filepath)
-      //       this.event.$emit('completeTask', name + '.' + suffix)
-      //     } else this.event.$emit('completeTask', name + '.' + suffix, false)
-      //   } else {
-      //     if (fs.existsSync(filepath)) this.event.$emit('completeTask', name)
-      //     else this.event.$emit('completeTask', name, false)
-      //   }
-      // }, () => {
-      //   this.current = 0
-      //   this.text = ''
-      // })
       this.total = 0
       this.downloadBtnDisable = false
-      if (errorList.length) this.event.$emit('alert', this.$t('home.download'), `Failed: ${errorList.length}`)
+      if (this.manualStop) {
+        this.manualStop = false
+      } else {
+        if (errorList.length) this.event.$emit('alert', this.$t('home.download'), `Failed: ${errorList.length}`)
+      }
     } else {
       this.event.$emit('alert', this.$t('home.errorTitle'), this.$t('home.noEmptyDownload'))
     }
