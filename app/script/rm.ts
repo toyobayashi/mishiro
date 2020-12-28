fbe6609d323ec29acfac26da0c186b33f23b2b06
toyobayashi <lifenglin314@outlook.com>
2020-12-28 16:20:38 +0800

    bump mishiro-core 4.8.0

--- app/script/rm.ts
+++ app/script/rm.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs-extra'
 import * as path from 'path'
 
-const nativeModules = ['hca-decoder', 'sqlite3', 'mishiro-core', 'spdlog']
+const nativeModules = ['hca-decoder', 'sqlite3', 'mishiro-core', 'spdlog', 'usm-decrypter']
 
 Promise.all(nativeModules.map(m => fs.remove(path.join(__dirname, '../node_modules', m)))).catch(e => console.log(e))
