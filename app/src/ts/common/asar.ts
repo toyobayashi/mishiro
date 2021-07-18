44f777ed5b72af66fd64b57e8b0fe18b3c4c2d3e
toyobayashi <lifenglin314@outlook.com>
2021-07-18 18:15:16 +0800

    eslint format

--- app/src/ts/common/asar.ts
+++ app/src/ts/common/asar.ts
@@ -7,30 +7,32 @@ import { join } from 'path'
 
   const originalResolveLookupPaths = Module._resolveLookupPaths
 
-  Module._resolveLookupPaths = originalResolveLookupPaths.length === 2 ? function (request: any, parent: any) {
-    const result = originalResolveLookupPaths(request, parent)
+  Module._resolveLookupPaths = originalResolveLookupPaths.length === 2
+    ? function (request: any, parent: any) {
+      const result = originalResolveLookupPaths(request, parent)
 
-    if (!result) return result
+      if (!result) return result
 
-    for (let i = 0; i < result.length; i++) {
-      if (result[i] === NODE_MODULES_PATH) {
-        result.splice(i, 0, NODE_MODULES_ASAR_PATH)
-        break
+      for (let i = 0; i < result.length; i++) {
+        if (result[i] === NODE_MODULES_PATH) {
+          result.splice(i, 0, NODE_MODULES_ASAR_PATH)
+          break
+        }
       }
+
+      return result
     }
+    : function (request: any, parent: any, newReturn: any) {
+      const result = originalResolveLookupPaths(request, parent, newReturn)
 
-    return result
-  } : function (request: any, parent: any, newReturn: any) {
-    const result = originalResolveLookupPaths(request, parent, newReturn)
-
-    const paths = newReturn ? result : result[1]
-    for (let i = 0; i < paths.length; i++) {
-      if (paths[i] === NODE_MODULES_PATH) {
-        paths.splice(i, 0, NODE_MODULES_ASAR_PATH)
-        break
+      const paths = newReturn ? result : result[1]
+      for (let i = 0; i < paths.length; i++) {
+        if (paths[i] === NODE_MODULES_PATH) {
+          paths.splice(i, 0, NODE_MODULES_ASAR_PATH)
+          break
+        }
       }
-    }
 
-    return result
-  }
+      return result
+    }
 })()
